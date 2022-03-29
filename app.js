4359f0ce95575773141c377b32cfcba28fb480bd
Dan Hadar <danhad@menoramivt.co.il>
2022-03-29 18:52:02 +0300

    added logs + try catch

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-// const baseUrl=process.argv[2]
-// const depth = process.argv[3]
-// const Crawler = require("crawler");
-// const utils = require("./utils");
-
-const baseUrl = "https://www.ynet.co.il/";
-const depth = 1;
 const CrawlerCls = require("./crawler");
 
-const c = new CrawlerCls(baseUrl, depth);
-console.log('Starting process');
-c.crawleUrl();
-// console.log(res);
+const baseUrl = process.argv[2] || "";
+const depth = +process.argv[3] || 0;
+try {
+  if (!baseUrl.length) throw Error("missing base url");
+  const c = new CrawlerCls(baseUrl, depth);
+  console.log(`Starting process: baseUrl = ${baseUrl}, max-depth = ${depth}`);
+  c.crawleUrl();
+} catch (err) {
+  console.error(
+    `app.js: Failed to crawle ${baseUrl} (depth: ${depth}) \nerr: ${err.stack}`
+  );
+}
--- crawler.js
+++ crawler.js
@@ -9,6 +9,7 @@ class CrawlerCls {
     this.maxDepth = depth || 0;
     this.finalResult = { results: [] };
     this.lastLink = "";
+    this.runningOnDepth = [];
   }
 
   crawleUrl = (url = this.baseUrl, depth = 0) => {
@@ -20,58 +21,73 @@ class CrawlerCls {
         try {
           this.getLinkAndImages(res.$, url, depth);
           done();
-          //   console.log(this.finalResult);
         } catch (err) {
-          console.log(err.message, err.stack);
+          console.error(
+            `crawler.js: Failed to add job to crawler queue, url: ${url} (depth: ${depth}) \nerr: ${err.stack}`
+          );
           done();
+          throw err; //if needed
         }
       },
     });
   };
 
   getLinkAndImages($, url, depth) {
-    if (depth < this.maxDepth) {
-      let links = [];
-      //recursive crawle links
-      let urls = $("a");
-      Object.keys(urls).forEach((item) => {
-        const currentTag = urls[item];
-        let href = currentTag?.attribs?.href?.trim();
-        if (
-          currentTag.type === "tag" &&
-          href?.startsWith(this.baseUrl) &&
-          !links.includes(href)
-        ) {
-          links.push(href);
+    try {
+      if (depth < this.maxDepth) {
+        let links = [];
+        //recursive crawle links
+        let urls = $("a");
+        Object.keys(urls).forEach((item) => {
+          const currentTag = urls[item];
+          let href = currentTag?.attribs?.href?.trim();
+          if (
+            currentTag.type === "tag" &&
+            href?.startsWith(this.baseUrl) &&
+            !links.includes(href)
+          ) {
+            links.push(href);
+          }
+        });
+        const nextDepth = depth + 1;
+        for (let i = 0; i < links.length; i++) {
+          const link = links[i];
+          this.crawleUrl(link, nextDepth);
+        }
+        if (nextDepth === this.maxDepth)
+          this.lastLink = links[links.length - 1];
+      }
+      if (!this.runningOnDepth.includes(depth)) {
+        this.runningOnDepth.push(depth);
+        console.log(`getting images from depth ${depth}`);
+      }
+      //images
+      let images = $("img");
+      Object.keys(images).forEach((item) => {
+        const currentTag = images[item];
+        if (currentTag?.type === "tag") {
+          this.finalResult.results = [
+            ...this.finalResult.results,
+            {
+              imageUrl: currentTag.attribs?.src,
+              sourceUrl: url,
+              depth,
+            },
+          ];
         }
       });
-      const nextDepth = depth + 1;
-      for (let i = 0; i < links.length; i++) {
-        //   for (let i = 0; i < 2; i++) {
-        const link = links[i];
-        this.crawleUrl(link, nextDepth);
+      if (url === this.lastLink || this.maxDepth === 0) {
+        console.log("finished process");
+        fs.writeFileSync(
+          "./finalObj.json",
+          JSON.stringify(this.finalResult, null, 2)
+        );
       }
-      if (nextDepth === this.maxDepth) this.lastLink = links[links.length - 1];
-    }
-    //images
-    let images = $("img");
-    Object.keys(images).forEach((item) => {
-      const currentTag = images[item];
-      if (currentTag?.type === "tag") {
-        this.finalResult.results = [
-          ...this.finalResult.results,
-          {
-            imageUrl: currentTag.attribs?.src,
-            sourceUrl: url,
-            depth,
-          },
-        ];
-      }
-    });
-
-    if (url === this.lastLink) {
-      fs.writeFileSync("./finalObj.json", JSON.stringify(this.finalResult, null, 2));
-      console.log("finished process");
+    } catch (err) {
+      console.error(
+        `crawler.js: Failed to get links or images from url: ${url} (depth: ${depth}) \nerr: ${err.stack}`
+      );
+      throw err; //if needed
     }
   }
 }
